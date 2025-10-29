882acff75c9905949c6c50d82f317a2aa617b86b
John Vilela <johnvvilela@gmail.com>
2025-10-29 19:03:14 -0300

    Add lazydocker installation script

---
+++ terminal/lazydocker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+log "INFO" "Installing Lazydocker"
+
+cd /tmp
+LAZYDOCKER_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazydocker/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+silent_log curl -sLo lazydocker.tar.gz "https://github.com/jesseduffield/lazydocker/releases/latest/download/lazydocker_${LAZYDOCKER_VERSION}_Linux_x86_64.tar.gz"
+tar -xf lazydocker.tar.gz lazydocker
+silent_log sudo install lazydocker /usr/local/bin
+rm lazydocker.tar.gz lazydocker
+cd -
+
+log "SUCCESS" "Lazydocker installed"
