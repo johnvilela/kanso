5a7fce6a2a525edeac5802a845577c230951162e
John Vilela <johnvvilela@gmail.com>
2025-10-29 20:10:32 -0300

    Set mise on the config.fish

--- simple.sh
+++ simple.sh
@@ -3,11 +3,11 @@
 log "INFO" "Executing SIMPLE"
 
 source "$KANSO_REPO/terminal/essential-packages.sh"
+source "$KANSO_REPO/terminal/fish.sh"
 source "$KANSO_REPO/terminal/zellij.sh"
 source "$KANSO_REPO/terminal/lazygit.sh"
 source "$KANSO_REPO/terminal/lazydocker.sh"
 source "$KANSO_REPO/terminal/lazyvim.sh"
 source "$KANSO_REPO/terminal/mise.sh"
-source "$KANSO_REPO/terminal/fish.sh"
 
 log "SUCCESS" "Installed SIMPLE"
--- terminal/mise.sh
+++ terminal/mise.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-silent_log wget -q https://mise.run/fish | sh
+silent_log wget -q https://mise.run | sh
 
 if [[ -f "$HOME/.local/bin/mise" ]]; then
     eval "$($HOME/.local/bin/mise activate bash)"
@@ -10,4 +10,6 @@ if command_exists mise; then
     log "INFO" "Installing Node.js via Mise"
     silent_log mise install --yes nodejs@20.19.0
     silent_log mise use nodejs@20.19.0 --global
+
+    echo 'mise activate fish | source' >>~/.config/fish/config.fish
 fi
