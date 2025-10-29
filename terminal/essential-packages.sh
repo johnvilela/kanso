7d837d5145bcd1104b8f0a82d398b3eab0fda0e3
John Vilela <johnvvilela@gmail.com>
2025-10-29 18:54:47 -0300

    Add essential-packages installation script

---
+++ terminal/essential-packages.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+log "INFO" "Adding repositories"
+
+PPAS=(
+    "universe"
+    "ppa:git-core/ppa"
+    "ppa:agornostal/ulauncher"
+)
+
+if ! apt-cache policy fish | grep -q "Candidate.*[0-9]"; then
+    PPAS+=("ppa:fish-shell/release-4")
+fi
+
+for ppa in "${PPAS[@]}"; do
+    silent_log sudo add-apt-repository -y "$ppa"
+done
+
+log "INFO" "Updating and upgrading system packages"
+
+silent_log sudo apt-get -q update
+silent_log sudo apt-get -yq upgrade
+
+log "INFO" "Installing essential packages"
+
+PACKAGES=(
+    # Build toolchain
+    build-essential gcc g++ make automake autoconf pkg-config
+    cmake ninja-build gettext unzip zip htop btop gpg libfontconfig1-dev
+    libxcb-xfixes0-dev libxkbcommon-dev dbus-x11 pipx node-typescript
+
+    # Networking/tools
+    curl git
+
+    # Crypto/SSL
+    libssl-dev
+
+    # Shells/UX
+    fish
+
+    # Apps/runtime
+    flatpak
+
+    # Dev tools
+    flameshot eza fzf bat tldr neovim
+
+    # Gnome extensions
+    gnome-tweak-tool gnome-sushi gnome-shell-extensions gnome-shell-extension-manager pipx
+)
+
+silent_log sudo apt-get install -yq --no-install-recommends "${PACKAGES[@]}"
+
+log "SUCCESS" "Essential packages installed"
