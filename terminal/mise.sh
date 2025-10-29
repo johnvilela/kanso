dc0db6a6004f57da2381e0671aefe6e0ad17b659
John Vilela <johnvvilela@gmail.com>
2025-10-29 19:04:17 -0300

    Add mise installation script

---
+++ terminal/mise.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+silent_log wget -qO https://mise.run/fish | sh
+
+if [[ -f "$HOME/.local/bin/mise" ]]; then
+    eval "$($HOME/.local/bin/mise activate bash)"
+fi
+
+if command_exists mise; then
+    log "INFO" "Installing Node.js via Mise"
+    silent_log mise install --yes nodejs@20.19.0
+    silent_log mise use nodejs@20.19.0 --global
+fi
