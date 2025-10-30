55de4977bc9fd29972f8207dce761c85d4b8a7bc
John Vilela <johnvvilela@gmail.com>
2025-10-29 23:25:39 -0300

    Create exec file to centralize the customization

---
+++ customization/exec.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source "$KANSO_REPO/customization/config-fish.sh"
+source "$KANSO_REPO/customization/dotfiles.sh"
+source "$KANSO_REPO/customization/ssh.sh"
+source "$KANSO_REPO/customization/projects.sh"
--- simple.sh
+++ simple.sh
@@ -3,5 +3,6 @@
 log "INFO" "Executing SIMPLE"
 
 source "$KANSO_REPO/terminal/exec.sh"
+source "$KANSO_REPO/customization/exec.sh"
 
 log "SUCCESS" "Installed SIMPLE"
