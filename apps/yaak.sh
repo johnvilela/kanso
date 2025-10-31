2bea4c3b17206aae0b2b0c3115e0f45d444ecdc7
John Vilela <johnvvilela@gmail.com>
2025-10-30 21:08:37 -0300

    Create yaak installation script

---
+++ apps/yaak.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+log "INFO" "Installing Yaak"
+
+cd "${REPO_ROOT}/tmp"
+wget -qO yaak_2025.5.6_amd64.deb https://github.com/mountain-loop/yaak/releases/download/v2025.5.6/yaak_2025.5.6_amd64.deb
+$SUDO dpkg -i yaak_2025.5.6_amd64.deb >/dev/null 2>&1
+rm yaak_2025.5.6_amd64.deb
+cd ..
+
+log "SUCCESS" "Yaak installed and configured"
