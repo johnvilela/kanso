beea691dde384213fe02f677bd027a60ff61eb33
John Vilela <johnvvilela@gmail.com>
2025-10-30 23:29:27 -0300

    Fix openfortivpn installation

--- apps/openfortivpn.sh
+++ apps/openfortivpn.sh
@@ -3,8 +3,11 @@
 log "INFO" "Installing OpenFortiVPN"
 
 cd /tmp
+mkdir -p openfortivpn
 wget -qO openfortivpn.zip https://github.com/adrienverge/openfortivpn/archive/refs/tags/v1.23.1.zip
-unzip -q openfortivpn.zip -d openfortivpn
+unzip -q openfortivpn.zip -d openfortivpntmp
+mv openfortivpntmp/*/* openfortivpn/*
+rm -rf openfortivpntmp
 cd /tmp/openfortivpn
 
 ./autogen.sh
