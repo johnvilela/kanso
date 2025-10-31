6c9e5b7dab069f50bb6debb4bf9cc36273254c7e
John Vilela <johnvvilela@gmail.com>
2025-10-30 21:21:24 -0300

    Fix /tmp path of two scripts

--- apps/openfortivpn.sh
+++ apps/openfortivpn.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-cd "${REPO_ROOT}/tmp"
+log "INFO" "Installing OpenFortiVPN"
+
+cd /tmp
 wget -qO openfortivpn.zip https://github.com/adrienverge/openfortivpn/archive/refs/tags/v1.23.1.zip
 unzip -q openfortivpn.zip -d openfortivpn
-cd "${REPO_ROOT}/tmp/openfortivpn"
+cd /tmp/openfortivpn
 
 ./autogen.sh
 ./configure --prefix=/usr/local --sysconfdir=/etc
 make
 sudo make install
+cd -
+
+log "SUCCESS" "OpenFortiVPN installed"
--- apps/yaak.sh
+++ apps/yaak.sh
@@ -2,10 +2,10 @@
 
 log "INFO" "Installing Yaak"
 
-cd "${REPO_ROOT}/tmp"
+cd /tmp
 wget -qO yaak_2025.5.6_amd64.deb https://github.com/mountain-loop/yaak/releases/download/v2025.5.6/yaak_2025.5.6_amd64.deb
 $SUDO dpkg -i yaak_2025.5.6_amd64.deb >/dev/null 2>&1
 rm yaak_2025.5.6_amd64.deb
-cd ..
+cd -
 
 log "SUCCESS" "Yaak installed and configured"
