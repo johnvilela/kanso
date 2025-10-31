8ebe7fa89b8158c5f4b659f23ef2ca295889ea6c
John Vilela <johnvvilela@gmail.com>
2025-10-30 22:49:59 -0300

    Fix typo error on variable

--- apps/alacritty.sh
+++ apps/alacritty.sh
@@ -6,14 +6,14 @@ silent_log sudo apt-get install -yqq alacritty
 
 mkdir -p ~/.config/alacritty
 
-sudo cp "${REPO_ROOT}/configs/alacritty/alacritty-term.svg" /usr/share/pixmaps/Alacritty.svg
-sudo desktop-file-install "${REPO_ROOT}/configs/alacritty/Alacritty.desktop"
+sudo cp "$KANSO_REPO/configs/alacritty/alacritty-term.svg" /usr/share/pixmaps/Alacritty.svg
+sudo desktop-file-install "$KANSO_REPO/configs/alacritty/Alacritty.desktop"
 
-sudo cp "${REPO_ROOT}/configs/alacritty/alacritty.toml" ~/.config/alacritty/alacritty.toml
+sudo cp "$KANSO_REPO/configs/alacritty/alacritty.toml" ~/.config/alacritty/alacritty.toml
 sudo update-desktop-database
 
 sudo update-alternatives --set x-terminal-emulator /usr/bin/alacritty
 
-cd "${REPO_ROOT}"
+cd "$KANSO_REPO"
 
 log "SUCCESS" "Alacritty installed and configured"
--- apps/yaak.sh
+++ apps/yaak.sh
@@ -4,7 +4,7 @@ log "INFO" "Installing Yaak"
 
 cd /tmp
 wget -qO yaak_2025.5.6_amd64.deb https://github.com/mountain-loop/yaak/releases/download/v2025.5.6/yaak_2025.5.6_amd64.deb
-$SUDO dpkg -i yaak_2025.5.6_amd64.deb >/dev/null 2>&1
+sudo dpkg -i yaak_2025.5.6_amd64.deb >/dev/null 2>&1
 rm yaak_2025.5.6_amd64.deb
 cd -
 
