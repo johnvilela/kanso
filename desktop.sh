a87ab1b5cbc1e8f39bddffce0289efb7cc286d74
John Vilela <johnvvilela@gmail.com>
2025-10-30 21:27:44 -0300

    Add desktop step to desktop.sh & settings to avoid the machine to turn-off

--- desktop.sh
+++ desktop.sh
@@ -2,6 +2,9 @@
 
 log "INFO" "Executing DESKTOP"
 
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+gsettings set org.gnome.desktop.session idle-delay 0
+
 # TERMINAL STEPS
 source "$KANSO_REPO/terminal/essential-packages.sh"
 source "$KANSO_REPO/terminal/fish.sh"
@@ -24,5 +27,12 @@ source "$KANSO_REPO/customization/config-fish.sh"
 source "$KANSO_REPO/customization/dotfiles.sh"
 source "$KANSO_REPO/customization/ssh.sh"
 source "$KANSO_REPO/customization/projects.sh"
+
+# DESKTOP STEP
+source "$KANSO_REPO/desktop/gnome-settings.sh"
+source "$KANSO_REPO/desktop/font.sh"
+
+gsettings set org.gnome.desktop.screensaver lock-enabled true
+gsettings set org.gnome.desktop.session idle-delay 300
 
 log "SUCCESS" "Installed DESKTOP"
