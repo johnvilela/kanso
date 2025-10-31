85e0c1c50bf8075967f45f1c22cc1a23ae97b31d
John Vilela <johnvvilela@gmail.com>
2025-10-30 23:51:42 -0300

    Make openfortivpn installation quiet

--- apps/openfortivpn.sh
+++ apps/openfortivpn.sh
@@ -6,14 +6,14 @@ cd /tmp
 mkdir -p openfortivpn
 wget -qO openfortivpn.zip https://github.com/adrienverge/openfortivpn/archive/refs/tags/v1.23.1.zip
 unzip -q openfortivpn.zip -d openfortivpntmp
-mv openfortivpntmp/*/* openfortivpn/*
+mv openfortivpntmp/*/* openfortivpn
 rm -rf openfortivpntmp
 cd /tmp/openfortivpn
 
-./autogen.sh
-./configure --prefix=/usr/local --sysconfdir=/etc
-make
-sudo make install
+silent_log bash -c "./autogen.sh"
+silent_log bash -c "./configure --prefix=/usr/local --sysconfdir=/etc"
+silent_log bash -c "make"
+silent_log bash -c "sudo make install"
 cd -
 
 log "SUCCESS" "OpenFortiVPN installed"
